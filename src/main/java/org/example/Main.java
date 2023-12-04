a181253f9a41e666b0cadb242e6fcbb45bf8d307
108 <108@academicos.cesde.edu.co>
2023-12-04 15:34:29 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Entradas del problema
+        String nombreusuario="juan carlos";
+        short edadusuario=34;
+        String telefonousuario="3206462574";
+        String documentoidentidad= "1020412519";
+        int cuotaenero;
+        int cuotafebrero;
+        int cuotamarzo;
+        int cuotaabril;
+        int cuotamayo;
+        int cuotajunio;
+        int cuotajulio;
+        int cuotaagosto;
+        int cuotaseptimbre;
+        int cuotaoctubre;
+        int cuotanoviembre;
+        String direccionusuario="Cll 50# 80-20";
+        double costoinscripcion=150000.0;
+        double tasadeinteresNatillera=0.1;
+        double saldofinalusuario;
+
+        //PROCESO
+
+        Scanner leerdato = new Scanner(System.in);
+
+        System.out.println("hola soy una IA ,cual es tu nombre");
+        leerdato.nextLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
